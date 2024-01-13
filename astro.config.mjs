556b1c95afa4c3443afea50874d67becdd3bdd7a
lucianpastures <lucianpastures23@outlook.com>
2024-01-12 18:38:06 -0600

    Format files w/ prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://polite-rugelach-69d56b.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact()],
 });
