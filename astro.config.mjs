928a52a23baa1b2e88d4343492e5ee3d5fbae33c
lucianpastures <lucianpastures23@outlook.com>
2024-01-14 17:05:59 -0600

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://polite-rugelach-69d56b.netlify.app/",
-  integrations: [preact()],
+  integrations: [preact(), tailwind()],
 });
